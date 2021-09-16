d1f0ba400155b7a909a8989ef98334f1906e8dfe
Biblbrox <biblbroxxx@gmail.com>
2021-09-16 16:32:45 +0300

    Initial commit

---
+++ SyncSVN/syncsvn.cs
@@ -0,0 +1,283 @@
+﻿//using ConfigLib;
+using SharpSvn;
+using System;
+using System.Collections.Generic;
+
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+
+// подключить nuget-пакет SharpSvn.1.9-x64
+namespace RepositoryLib
+{
+    [DisplayName("Настройки хранилища файлов (SVN)")]
+    public class SVNFileRepositoryConfig
+    {
+        public SVNFileRepositoryConfig()
+        {
+            configData = new SortedDictionary<string, string>();
+            //AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", configPath)
+            parseConfigFile();
+        }
+
+        public void parseConfigFile()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                if (appSettings.Count == 0)
+                {
+                    throw new ConfigurationErrorsException("Application settings file doesn't exists");
+                }
+                else
+                {
+                    foreach (var key in appSettings.AllKeys)
+                    {
+                        configData.Add(key, appSettings[key].ToString());
+                    }
+                }
+            }
+            catch (ConfigurationException e)
+            {
+                throw new ConfigurationErrorsException("Error reading app settings: " + e.Message);
+            }
+        }
+
+        [Category("Место хранения")]
+        [DisplayName("Локальная папка")]
+        public string RootPath { get; set; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments), "RVRFiles");[Category("Место хранения")]
+        //[DisplayName("Удаленная папка")]
+        //public string SvnUrl { get; set; } = @"https://DESKTOP-TR8E3OJ/svn/test_repo/";
+        //[Category("Авторизация")]
+        //[DisplayName("Имя пользователя")]
+        //public string SvnUser { get; set; } = "aleksey";
+        //[Category("Авторизация")]
+        //[DisplayName("Пароль пользователя")]
+        //public string SvnPassword { get; set; } = "82348234";
+        public SortedDictionary<string, string> configData { get; set; }
+    }
+
+    public class SVNFileRepository// : IFileRepository
+    {
+        private SvnClient client;
+
+        public static string GenerateFilename(string path)
+        {
+            string fileName = Path.GetFileName(path);
+            string result = $"{DateTime.Now:yyyyMMddHHmmss}_{fileName}";
+            return result;
+        }
+
+        public SVNFileRepositoryConfig Config { get; private set; }
+        /*public SVNFileRepository(IConfigManager manager) : this(manager.Get<SVNFileRepositoryConfig>(ConfigType.SVNFileRepository)) 
+        {
+        
+        }*/
+
+        //protected SVNFileRepository(SVNFileRepositoryConfig config)
+        public SVNFileRepository(SVNFileRepositoryConfig config)
+
+        {
+            Config = config;
+            client = InitSvnClient();
+        }
+
+        public string Download(string id)
+        {
+            lock (client)
+            {
+                var svnFolder = Config.RootPath;
+                var svnUrl = Config.configData["SvnUrl"];
+                if (!Directory.Exists(svnFolder))
+                    Directory.CreateDirectory(svnFolder);
+                var svnPath = Path.Combine(svnFolder, id);
+                if (File.Exists(svnPath))
+                    return svnPath;
+
+                var checkoutArgs = new SvnCheckOutArgs {/* Depth = SvnDepth.Empty*/ };
+                //using (var client = InitSvnClient())
+                //{
+                try
+                {
+                    if (client.GetUrl(svnFolder).IndexOf(svnUrl) < 0)
+                    {
+                        client.CheckOut(new SvnUriTarget(svnUrl), svnFolder, checkoutArgs);
+                    }
+                    client.Update(svnPath);
+                }
+                catch
+                {
+                    throw new Exception("Unable to fetch content from repository");
+                }
+                return svnPath;
+                //}
+            }
+        }
+        public SvnClient InitSvnClient()
+        {
+            SvnClient client = null;
+            try
+            {
+                client = new SvnClient();
+                client.Authentication.ForceCredentials(Config.configData["SvnUser"], Config.configData["SvnPassword"]);
+                client.Authentication.SslServerTrustHandlers += (s, e) =>
+                {
+                    e.AcceptedFailures = e.Failures;
+                    e.Save = true;
+                };
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Unable to connect to svn server: " + e.Message);
+            }
+
+            return client;
+        }
+
+        public string Upload(string path)
+        {
+            lock (client)
+            {
+                var svnFolder = Config.RootPath;
+                var svnUrl = Config.configData["SvnUrl"];
+                if (!Directory.Exists(svnFolder))
+                    Directory.CreateDirectory(svnFolder);
+                if (!File.Exists(path))
+                    return string.Empty;
+                var fileName = GenerateFilename(path);
+                var svnPath = Path.Combine(svnFolder, fileName);
+                var checkoutArgs = new SvnCheckOutArgs { Depth = SvnDepth.Empty };
+                var commitArgs = new SvnCommitArgs { LogMessage = $"Add {path} to {svnPath}" };
+                File.Copy(path, svnPath);
+                //using (var client = InitSvnClient())
+                //{
+                try
+                {
+                    if (client.GetUrl(svnFolder).IndexOf(svnUrl) < 0)
+                        client.CheckOut(new SvnUriTarget(svnUrl), svnFolder, checkoutArgs);
+                    client.Add(svnPath);
+                    client.Commit(svnFolder, commitArgs);
+                }
+                catch { }
+                return fileName;
+                //}
+            }
+        }
+
+        public void Pull()
+        {
+            lock (client)
+            {
+                var svnFolder = Config.RootPath;
+                var svnUrl = Config.configData["SvnUrl"];
+                if (!Directory.Exists(svnFolder))
+                    Directory.CreateDirectory(svnFolder);
+
+                var checkoutArgs = new SvnCheckOutArgs { /*Depth = SvnDepth.Empty*/ };
+                try
+                {
+                    if (client.GetUrl(svnFolder).IndexOf(svnUrl) < 0)
+                    {
+                        //                        client.CheckOut(new SvnUriTarget(svnUrl), svnFolder, checkoutArgs);
+
+                        SvnUpdateArgs args = new SvnUpdateArgs();
+                        args.Revision = SvnRevision.Head;
+                        Console.Write("Pull changes to directory " + svnFolder + " with revision " + args.Revision + "\n");
+
+
+                        client.CheckOut(new SvnUriTarget(svnUrl), svnFolder, checkoutArgs);
+                    }
+
+                    bool gotList;
+                    List<string> files = new List<string>();
+                    System.Collections.ObjectModel.Collection<SvnListEventArgs> list;
+                    gotList = client.GetList(svnFolder, out list);
+                    if (gotList)
+                    {
+                        foreach (SvnListEventArgs item in list)
+                        {
+                            client.Update(Path.Combine(svnFolder, item.Path), new SvnUpdateArgs() { UpdateParents = true });
+                            files.Add(item.Path);
+                        }
+                    }
+                    client.Update(svnFolder, new SvnUpdateArgs() { UpdateParents = true });
+                }
+                catch
+                {
+                    throw new Exception("Unable to pull content from repository");
+                }
+            }
+        }
+
+        public void Push()
+        {
+            lock (client)
+            {
+                var svnFolder = Config.RootPath;
+                var svnUrl = Config.configData["SvnUrl"];
+                if (!Directory.Exists(svnFolder))
+                    Directory.CreateDirectory(svnFolder);
+
+                var checkoutArgs = new SvnCheckOutArgs { /*Depth = SvnDepth.Empty*/ };
+                try
+                {
+                    if (client.GetUrl(svnFolder).IndexOf(svnUrl) < 0)
+                    {
+                        //                        client.CheckOut(new SvnUriTarget(svnUrl), svnFolder, checkoutArgs);
+
+                        SvnUpdateArgs args = new SvnUpdateArgs();
+                        args.Revision = SvnRevision.Head;
+                        Console.Write("Pull changes to directory " + svnFolder + " with revision " + args.Revision + "\n");
+
+
+                        client.CheckOut(new SvnUriTarget(svnUrl), svnFolder, checkoutArgs);
+                    }
+
+
+                    client.Log(
+                        svnFolder,
+                        new SvnLogArgs
+                        {
+                            Range = new SvnRevisionRange(9999, 9999)
+                        },
+                        (o, e) =>
+                        {
+                            foreach (var changeItem in e.ChangedPaths)
+                            {
+                                Console.WriteLine(string.Format( "{0} {1} {2} {3}", changeItem.Action,
+                                       changeItem.CopyFromRevision,
+                                       changeItem.CopyFromPath));
+                            }
+                        });
+                }
+                catch
+                {
+                    throw new Exception("Unable to pull content from repository");
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            client?.Dispose();
+        }
+    }
+    public static class SvnExt
+    {
+        public static string GetUrl(this SvnClient client, string folder)
+        {
+            if (client != null)
+            {
+                var svnUri = client.GetUriFromWorkingCopy(folder);
+                if (svnUri == null) return string.Empty;
+                return svnUri.AbsoluteUri;
+            }
+            return "";
+        }
+    }
+}
---
+++ SyncSVNTests/SVNFileRepositoryTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepositoryLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLib.Tests
+{
+    [TestClass()]
+    public class SVNFileRepositoryTests
+    {
+        /**
+         * Test download specific file
+         */
+        [TestMethod()]
+        public void DownloadTest()
+        {
+            try
+            {
+                var config = new RepositoryLib.SVNFileRepositoryConfig();
+
+                System.Diagnostics.Trace.WriteLine("Config loaded");
+                // Used config
+                Console.WriteLine("User: " + config.configData["SvnUser"]);
+                Console.WriteLine("Password: " + config.configData["SvnPassword"]);
+                Console.WriteLine("Server: " + config.configData["SvnUrl"]);
+
+
+                RepositoryLib.SVNFileRepository repo = null;
+                try
+                {
+                    repo = new RepositoryLib.SVNFileRepository(config);
+                }
+                catch (Exception e)
+                {
+                    Assert.Fail("Unable to init repository: " + e.Message + "\n");
+                }
+
+                System.Diagnostics.Trace.WriteLine("Succesfully init svn client");
+
+                // Test downoad file
+                repo.Download("asd.txt");
+                repo.Download("New_File.txt");
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Exception occured: " + e.Message + "\n");
+            }
+        }
+
+        /**
+         * Test pull content from remove repository
+         */
+        [TestMethod()]
+        public void PullTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
